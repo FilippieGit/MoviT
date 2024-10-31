112e5da72453c4d252b09abd166849814c1b435d
FilippieGit <filippieamaral@gmail.com>
2024-10-31 20:25:20 -0300

    v1

---
+++ app/src/main/java/br/com/etecia/movit/Adapter.java
@@ -0,0 +1,37 @@
+package br.com.etecia.movit;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private Context context;
+    private List<Movies> lstMovies;
+
+    public Adapter(Context context, List<Movies> lstMovies) {
+        this.context = context;
+        this.lstMovies = lstMovies;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/br/com/etecia/movit/Movies.java
@@ -0,0 +1,29 @@
+package br.com.etecia.movit;
+
+public class Movies {
+
+    private String name;
+
+    private int image;
+
+    public Movies(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
